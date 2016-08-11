ee7ecc4b6ebdf4633a6687fa72d20ca5720b67b9
Eric Westman <haldous2@gmail.com>
2016-08-11 13:33:53 -0700

    Update hash_table.py

--- hash_table.py
+++ hash_table.py
@@ -16,22 +16,21 @@ Hash Tables
 Using lists and lists within lists
 Note: could just use a dictionary, what fun would that be ?
 
-when adding, will always be a leaf - just follow the trail left or right until a spot is open
-when deleting, 3 scenarios will decide how element is removed
-  1. has two elements attached (is a node)
-  2. has one element attached
-  3. has no elements (is a leaf
-  )
+Hash a key to index value in list (array index)
+Store key, value pair in a list
+Update key, value pairs when match on hash and item
+Insert new list for all collision key, value pairs
+
 """
 
 class hashMap(object):
 
     def __init__(self):
-        self.size = 64
+        self.size = 6
         self.table = [None] * self.size
 
         ## test
-        self.table[3] = [['Eric', 'Westman']]
+        #self.table[3] = [['Eric', 'Westman']]
 
     def _get_hash(self, key):
         hash = 0
@@ -63,18 +62,32 @@ class hashMap(object):
             self.table[hash] = [[key, value]]
         else:
             # list(s) found - look for dupicates, add or update
-            i = 0
+
             dup = False
+
+            # range method
+            for i in range(0, len(self.table[hash])):
+                if self.table[hash][i][0] == key:
+                    #print "duplicate data{} data.0:{} = key:{}".format(self.table[hash][i],self.table[hash][i][0],key)
+                    dup = True
+                    break
+
+            # counter method
+            i = 0
             for data in self.table[hash]:
                 if data[0] == key:
+                    #print "duplicate data{} data.0:{} = key:{}".format(data,data[0],key)
                     dup = True
                     break
                 i += 1
+
             if dup is True:
                 # updating
+                #print "updating:{}@{}".format(key,i)
                 self.table[hash][i] = [key, value]
             else:
                 # adding
+                #print "adding:{}".format(key)
                 self.table[hash].append([key, value])
 
     def delete(self, key):
@@ -82,13 +95,23 @@ class hashMap(object):
         if hash is None:
             return None
         if self.table[hash] is not None:
-            i = 0
-            for data in self.table[hash]:
-                ## need to remove a list inside a list
-                if data[0] == key:
+
+            # range method
+            for i in range(0, len(self.table[hash])):
+                #print "delete:{},{},{}".format(hash, i, self.table[hash][i][0])
+                if self.table[hash][i][0] == key:
                     self.table[hash].pop(i)
                     break
-                i += 1
+
+            # counter method
+            #i = 0
+            #for data in self.table[hash]:
+                ## need to remove a list inside a list
+            #    if data[0] == key:
+            #        self.table[hash].pop(i)
+            #        break
+            #    i += 1
+
             # reset column
             if len(self.table[hash]) == 0:
                 self.table[hash] = None
@@ -98,23 +121,21 @@ class hashMap(object):
 
     def show(self):
         i = 0
-        for hash in self.table:
-            if hash is not None:
-                print "[{:03}]--{}".format(i, hash)
-            i += 1
+        for i in range(0, len(self.table)):
+            if self.table[i] is not None:
+                print "[{:03}]--{}".format(i, self.table[i])
 
 hs = hashMap()
 
+hs.add("Irce", "Mirksy")
 hs.add("Cire", "Delish")
 hs.add("Eric", "RovaLova")
 hs.add("Eric", "Vestmer")
 hs.add("Bob", "Zabowsky")
 hs.add("George", "Carlin")
 
-hs.delete("Eric")
+#hs.delete("Eric")
 
-print hs._get_value('Eric')
 print hs._get_value('Bob')
-print hs._get_value('George')
 
 hs.show()
