8b5e40adeb43718026524c774e6ae290781f6846
Eric Westman <haldous2@gmail.com>
2017-05-14 20:57:08 -0700

    Update ag_heap.py

--- ag_heap.py
+++ ag_heap.py
@@ -17,9 +17,9 @@ Note: heaps can be defined as arrays - since everything is binary split we can d
       children = (i * 2), (i * 2) + 1
       max      = heap[1]
 
-Example: Max Heap                 40
-                              35      38
-                            33  31  28  22
+Example: Max Heap                 40                   1
+                              35      38           2       3
+                            33  31  28  22       4   5   6   7
                           27  --
 
         As Array:         0 40 35 38 33 31 28 22 27 --
@@ -33,192 +33,90 @@ Example: Max Heap                 40
         As Array:        0 22 28 27 31 33 35 38 40 --
 """
 
-class maxHeap(object):
+class maxHeap:
 
-    def __init__(self, data):
-        self.data = data
+    """
+    Max Heap
+    note: index starts at 1, need to skip 0 since siblings calculated from index
+    """
 
-    def viewData(self):
-        return self.data
+    def __init__(self):
+        self.h = [-9999]
 
     def peek(self):
-        # just peeking! - nothing removed
-        if len(self.data) > 1:
-            return self.data[1]
-        else:
-            return False
+        return self.h[1]
 
     def pop(self):
+        self.h[1] = self.h[-1]
+        self.h.pop()
+        self.bubble_down(1)
 
-        # pop - return min
-        # swap top and bottom values
-        # bubble down top so heap is in max state
+    def push(self, value):
+        """
+        insert value at end of array (list)
+        """
+        self.h.append(value)
+        self.float_up(len(self.h) - 1)
 
-        # store value of max for return
-        i_val = self.peek()
-
-        # swap first and last element
-        self.__swap(1, len(self.data) - 1)
-
-        # remove last
-        self.data.pop()
-
-        # bubble down max to it's new home
-        #self.__bubbleDown(1)
-        self.__rBubbleDown(1)
-
-        return i_val
-
-    def push(self, v):
-        # insert at the end, regorganize
-        self.data.append(v)
-        i = len(self.data) - 1
-        self.__floatUp(i)
-
-    def initData(self):
-        # organize data high to low
-        for i, v in enumerate(self.data):
-            if i == 0 and self.data[i] == 0:
-                continue
-            self.__rBubbleDown(i)
-
-    def __swap(self, i, j):
-        if i == j:
+    def heapify(self, l):
+        """
+        heapify incoming list l
+        """
+        if not l:
             return
-        if len(self.data) <= 1:
-            return
-        self.data[i], self.data[j] = self.data[j], self.data[i]
+        for i in xrange(0, len(l)):
+            self.push(l[i])
 
-    def __floatUp(self, i):
+    def float_up(self, index=1):
+        """
+        float value at index up while smaller than parent
+        note: to keep 0th index clear, p > 0
+        """
 
-        # iterative float up
-        # i is child index, not value
+        i = index
+        p = index // 2
 
-        parent = i / 2 // 1 # floor of i/2
+        while self.h[i] > self.h[p] and p > 0:
+            self.h[p], self.h[i] = self.h[i], self.h[p]
+            i = p
+            p = i // 2
 
-        #print "parent [{}]:{} i [{}]:{}".format(parent, self.data[parent], i, self.data[i])
+    def bubble_down(self, index):
+
+        h_len = len(self.h) - 1
+
+        i = index
 
         while True:
-            if parent <= 0:
-                break
-            if self.data[parent] > self.data[i]:
-                break
-            self.__swap(i, parent)
-            i = parent
-            parent = i / 2 // 1
-
-    def __rFloatUp(self, i):
-
-        # recursive float up
-        # i is child index, not value
-
-        parent = i / 2 // 1 # floor of i/2
-
-        if parent <= 0:
-            return
-        if self.data[parent] > self.data[i]:
-            return
-        self.__swap(i, parent)
-        i = parent
-        self.__rFloatUp(i)
-
-    def __bubbleDown(self, i):
-
-        # bubble down when largest child is more than parent i
-        # i is parent index, not value
-
-        data_len = len(self.data) - 1
-
-        if data_len < 2:
-            return
-
-        if data_len == 2:
-
-            child_left = i * 2
-
-            if self.data[child_left] > self.data[i]:
-                self.__swap(i, child_left)
-
-        else:
 
             child_left = i * 2
             child_right = i * 2 + 1
 
-            #while self.data[i] < (self.data[child_left] or self.data[child_right]):
-            while True:
-
-                if child_left <= data_len:
-                    if child_right <= data_len:
-                        # both within bounds
-                        if self.data[child_left] > self.data[child_right]:
-                            self.__swap(i, child_left)
-                            i = child_left
-                        else:
-                            self.__swap(i, child_right)
-                            i = child_right
-                    else:
-                        # only left in bounds
-                        self.__swap(i, child_left)
-                        i = child_left
-                else:
-                    # neither one in bounds
-                    break
-
-                child_left = i * 2
-                child_right = i * 2 + 1
-
-                if (child_left or child_right) >= data_len:
-                    break
-
-    def __rBubbleDown(self, i):
-
-        # bubble down when largest child is more than parent i
-        # i is parent index, not value
-
-        data_len = len(self.data) - 1
-
-        if data_len < 2:
-            return
-
-        if data_len == 2:
-
-            child_left = i * 2
-
-            if self.data[child_left] > self.data[i]:
-                self.__swap(i, child_left)
-
-        else:
-
-            child_left = i * 2
-            child_right = i * 2 + 1
-
-            if child_left <= data_len:
-                if child_right <= data_len:
+            if child_left <= h_len:
+                if child_right <= h_len:
                     # both within bounds
-                    if self.data[child_left] > self.data[child_right]:
-                        if self.data[child_left] > self.data[i]:
-                            self.__swap(i, child_left)
+                    if self.h[child_left] > self.h[child_right]:
+                        if self.h[child_left] > self.h[i]:
+                            self.h[i], self.h[child_left] = self.h[child_left], self.h[i]
                             i = child_left
                         else:
                             return
                     else:
-                        if self.data[child_right] > self.data[i]:
-                            self.__swap(i, child_right)
+                        if self.h[child_right] > self.h[i]:
+                            self.h[i], self.h[child_right] = self.h[child_right], self.h[i]
                             i = child_right
                         else:
                             return
                 else:
                     # only left in bounds
-                    if self.data[child_left] > self.data[i]:
-                        self.__swap(i, child_left)
+                    if self.h[child_left] > self.h[i]:
+                        self.h[i], self.h[child_left] = self.h[child_left], self.h[i]
                         i = child_left
                     else:
                         return
             else:
                 # neither one in bounds
                 return
-
-            self.__rBubbleDown(i)
 
 maxData = [0,40,35,38,33,31,28,22,27,5]
 maxData = [0,40,35,38,33,31,28,22,27,5]
@@ -228,129 +126,97 @@ maxData = [0,40,35,38,33,31,28,22,27,5]
 #maxData = [0,40,35,38]
 #maxData = [0,40,35,38,33]
 
-theMaxHeap = maxHeap(maxData)
-print theMaxHeap.viewData()
+theMaxHeap = maxHeap()
+theMaxHeap.heapify(maxData)
+print theMaxHeap.h
 
 #print theMaxHeap.peek()
 #theMaxHeap.push(50)
 #print "peek:{}".format(theMaxHeap.peek())
 #print theMaxHeap.pop()
-theMaxHeap.initData()
 
-print theMaxHeap.viewData()
+print theMaxHeap.h
 
-class minHeap(object):
+class minHeap:
 
-    def __init__(self, data):
-        self.data = data
+    """
+    Min Heap
+    note: index starts at 1, need to skip 0 since siblings calculated from index
+    """
 
-    def viewData(self):
-        return self.data
+    def __init__(self):
+        self.h = [-9999]
 
     def peek(self):
-        # just peeking! - nothing removed
-        if len(self.data) > 1:
-            return self.data[1]
-        else:
-            return False
+        return self.h[1]
 
     def pop(self):
+        self.h[1] = self.h[-1]
+        self.h.pop()
+        self.bubble_down(1)
 
-        # pop - return min
-        # swap top and bottom values
-        # bubble down top so heap is in min state
+    def push(self, value):
+        """
+        insert value at end of array (list)
+        """
+        self.h.append(value)
+        self.float_up(len(self.h) - 1)
 
-        # store value of min for return
-        i_val = self.peek()
-
-        # swap first and last element
-        self.__swap(1, len(self.data) - 1)
-
-        # remove last
-        self.data.pop()
-
-        # bubble down max to it's new home
-        self.__rBubbleDown(1)
-
-        return i_val
-
-    def push(self, v):
-        # insert at the end, regorganize
-        self.data.append(v)
-        self.__rFloatUp(len(self.data) - 1)
-
-    def initData(self):
-        # organize data into low to high
-        for i, v in enumerate(self.data):
-            if i == 0 and self.data[i] == 0:
-                continue
-            self.__rBubbleDown(i)
-
-    def __swap(self, i, j):
-        if i == j:
+    def heapify(self, l):
+        """
+        heapify incoming list l
+        """
+        if not l:
             return
-        if len(self.data) <= 1:
-            return
-        self.data[i], self.data[j] = self.data[j], self.data[i]
+        for i in xrange(0, len(l)):
+            self.push(l[i])
 
-    def __rFloatUp(self, i):
+    def float_up(self, index=1):
+        """
+        float value at index up while smaller than parent
+        note: to keep 0th index clear, p > 0
+        """
+        i = index
+        p = index // 2
 
-        # recursive float up
-        # i is child index, not value
+        while self.h[i] < self.h[p] and p > 0:
+            self.h[p], self.h[i] = self.h[i], self.h[p]
+            i = p
+            p = i // 2
 
-        parent = i / 2 // 1 # floor of i/2
+    def bubble_down(self, index=1):
 
-        if parent <= 0:
-            return
-        if self.data[parent] < self.data[i]:
-            return
-        self.__swap(i, parent)
-        i = parent
-        self.__rFloatUp(i)
+        h_len = len(self.h) - 1
 
-    def __rBubbleDown(self, i):
+        i = index
 
-        # bubble down when parent larger than child, towards lesser child
-        # i is parent index, not value
-
-        #print "rBubbledown({}) value:{}".format(i, self.data[i])
-
-        data_len = len(self.data) - 1
-
-        if data_len < 2:
-            return
-
-        if data_len == 2:
-
-            child_left = i * 2
-
-            if self.data[child_left] < self.data[i]:
-                self.__swap(i, child_left)
-
-        else:
+        while True:
 
             child_left = i * 2
             child_right = i * 2 + 1
 
-            if child_left <= data_len:
-                if child_right <= data_len:
+            if child_left <= h_len:
+                if child_right <= h_len:
                     # both within bounds
-                    if self.data[child_left] < self.data[child_right]:
-                        if self.data[child_left] < self.data[i]:
-                            self.__swap(i, child_left)
+                    if self.h[child_left] < self.h[child_right]:
+                        if self.h[child_left] < self.h[i]:
+                            #print "swap left with parent...", self.h[child_left], self.h[i]
+                            self.h[i], self.h[child_left] = self.h[child_left], self.h[i]
                             i = child_left
                         else:
                             return
                     else:
-                        if self.data[child_right] < self.data[i]:
-                            self.__swap(i, child_right)
+                        if self.h[child_right] < self.h[i]:
+                            #print "swap right with parent...", self.h[child_right], self.h[i]
+                            self.h[i], self.h[child_right] = self.h[child_right], self.h[i]
                             i = child_right
                         else:
                             return
                 else:
                     # only left in bounds
-                    if self.data[child_left] < self.data[i]:
-                        self.__swap(i, child_left)
+                    if self.h[child_left] < self.h[i]:
+                        #print "swap left with parent...", self.h[child_left], self.h[i]
+                        self.h[i], self.h[child_left] = self.h[child_left], self.h[i]
                         i = child_left
                     else:
                         return
@@ -358,18 +224,17 @@ class minHeap(object):
                 # neither one in bounds
                 return
 
-            self.__rBubbleDown(i)
-
 minData = [0,22,28,27,33,38,40,31,35] # sorted
 #minData = [0,33,22,40,28,38,27,31,35] # scrambled eggs
 
-theMinHeap = minHeap(minData)
-print "min data - start:{}".format(theMinHeap.viewData())
-#theMinHeap.initData() # recombobulate the data in the right order
-#print "min data - sorted:{}".format(theMinHeap.viewData())
+theMinHeap = minHeap()
+theMinHeap.heapify(minData)
+print "min data - start:{}".format(theMinHeap.h)
+
+#print "min data - sorted:{}".format(theMinHeap.h)
 #print "min:{}".format(theMinHeap.pop())
 print "adding 50"
 theMinHeap.push(50)
 print "adding 18"
 theMinHeap.push(18)
-print "min data - end:{}".format(theMinHeap.viewData())
+print "min data - end:{}".format(theMinHeap.h)
